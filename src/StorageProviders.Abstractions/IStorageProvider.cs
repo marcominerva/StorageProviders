3ca38084ad4f8af10005b366f4462cd4bcc550e3
Marco Minerva <marco.minerva@gmail.com>
2022-12-27 11:19:25 +0100

    Add CancellationToken support #6

--- src/StorageProviders.Abstractions/IStorageProvider.cs
+++ src/StorageProviders.Abstractions/IStorageProvider.cs
@@ -2,36 +2,39 @@
 
 public interface IStorageProvider
 {
-    async Task SaveAsync(string path, byte[] content, bool overwrite = false)
+    async Task SaveAsync(string path, byte[] content, bool overwrite = false, CancellationToken cancellationToken = default)
     {
         using var stream = new MemoryStream(content);
-        await SaveAsync(path, stream, overwrite).ConfigureAwait(false);
+        await SaveAsync(path, stream, overwrite, cancellationToken).ConfigureAwait(false);
     }
 
-    Task SaveAsync(string path, Stream stream, bool overwrite = false);
+    Task SaveAsync(string path, Stream stream, bool overwrite = false, CancellationToken cancellationToken = default);
 
-    Task<Stream?> ReadAsStreamAsync(string path);
+    Task<Stream?> ReadAsStreamAsync(string path, CancellationToken cancellationToken = default);
 
-    async Task<byte[]?> ReadAsByteArrayAsync(string path)
+    async Task<byte[]?> ReadAsByteArrayAsync(string path, CancellationToken cancellationToken = default)
     {
-        using var stream = await ReadAsStreamAsync(path).ConfigureAwait(false);
+        using var stream = await ReadAsStreamAsync(path, cancellationToken).ConfigureAwait(false);
         if (stream is not null)
         {
             using var memoryStream = new MemoryStream();
-            await stream.CopyToAsync(memoryStream).ConfigureAwait(false);
+            await stream.CopyToAsync(memoryStream, cancellationToken).ConfigureAwait(false);
             return memoryStream.ToArray();
         }
 
         return null;
     }
 
-    Task<Uri> GetFullPathAsync(string path);
+    Task<Uri> GetFullPathAsync(string path, CancellationToken cancellationToken = default);
 
-    Task<Uri?> GetReadAccessUriAsync(string path, DateTime expirationDate);
+    Task<Uri?> GetReadAccessUriAsync(string path, DateTime expirationDate, CancellationToken cancellationToken = default);
 
-    Task<bool> ExistsAsync(string path);
+    Task<bool> ExistsAsync(string path, CancellationToken cancellationToken = default);
 
-    IAsyncEnumerable<string> EnumerateAsync(string? prefix = null, params string[] extensions);
+    IAsyncEnumerable<string> EnumerateAsync(string? prefix = null, params string[] extensions)
+        => EnumerateAsync(prefix, extensions, CancellationToken.None);
 
-    Task DeleteAsync(string path);
+    IAsyncEnumerable<string> EnumerateAsync(string? prefix, string[] extensions, CancellationToken cancellationToken);
+
+    Task DeleteAsync(string path, CancellationToken cancellationToken = default);
 }
