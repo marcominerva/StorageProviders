ddd3120ae8ca7d18004514ff3e9556dff3109de0
Marco Minerva <marco.minerva@gmail.com>
2024-06-26 10:48:17 +0200

    Refactor to minimal APIs and add OpenAPI
    - Updated `AzureStorageSample.csproj` by adding `Microsoft.AspNetCore.OpenApi` version `8.0.6` to introduce OpenAPI support for enhanced API documentation and testing.
    - Removed `AttachmentsController.cs` to transition from traditional MVC controllers to minimal APIs, aligning with .NET 6+ trends for simplified API development.
    - Deleted `GlobalSuppressions.cs`, indicating a move towards addressing compiler and code analysis warnings directly in the code.
    - Overhauled `Program.cs`:
      * Implemented minimal API endpoints for attachment operations, replacing traditional MVC controller routes.
      * Added `using` statements for namespaces essential for minimal API functionality and OpenAPI support.
      * Reconfigured services to support minimal APIs, including form file handling, default responses, problem details, and exception handling.
      * Replaced `MapControllers` with specific minimal API route mappings under `/api/attachments`, fully supported by OpenAPI for automatic API documentation inclusion.
      * Configured endpoints with specific settings, such as disabling antiforgery for the POST operation.
    - This commit represents a significant refactoring towards modernizing the application, enhancing scalability, developer productivity, and API documentation.

--- samples/AzureStorageSample/Program.cs
+++ samples/AzureStorageSample/Program.cs
@@ -1,21 +1,22 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
+using MimeMapping;
+using MinimalHelpers.OpenApi;
+using StorageProviders;
 using StorageProviders.AzureStorage;
+using TinyHelpers.AspNetCore.Extensions;
+using TinyHelpers.AspNetCore.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "Azure Storage API", Version = "v1" });
 
-    options.MapType<FileContentResult>(() => new()
-    {
-        Type = "file"
-    });
+    options.AddFormFile();
+    options.AddDefaultResponse();
 });
 
 builder.Services.AddAzureStorage(options =>
@@ -24,10 +25,14 @@ builder.Services.AddAzureStorage(options =>
     options.ContainerName = builder.Configuration.GetValue<string>("AppSettings:ContainerName");
 });
 
-builder.Services.AddProblemDetails();
+builder.Services.AddDefaultProblemDetails();
+builder.Services.AddDefaultExceptionHandler();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
+app.UseStatusCodePages();
 
 app.UseSwagger();
 app.UseSwaggerUI(options =>
@@ -36,6 +41,78 @@ app.UseSwaggerUI(options =>
     options.RoutePrefix = string.Empty;
 });
 
-app.MapControllers();
+var attachementsApiGroup = app.MapGroup("/api/attachments");
+
+attachementsApiGroup.MapGet(string.Empty, (IStorageProvider storageProvider, string? prefix = null, [FromQuery(Name = "extension")] string[] extensions = null!) =>
+{
+    var attachments = storageProvider.EnumerateAsync(prefix, extensions);
+
+    // If you need to get the actual list, you can use the .ToListAsync() extension method:
+    //var list = await attachments.ToListAsync();
+
+    return TypedResults.Ok(attachments);
+})
+.WithOpenApi();
+
+attachementsApiGroup.MapGet("exists", async Task<Results<NoContent, NotFound>> (IStorageProvider storageProvider, string fileName) =>
+{
+    var exists = await storageProvider.ExistsAsync(fileName);
+    if (!exists)
+    {
+        return TypedResults.NotFound();
+    }
+
+    return TypedResults.NoContent();
+})
+.WithOpenApi();
+
+attachementsApiGroup.MapGet("full-path", async (IStorageProvider storageProvider, string fileName) =>
+{
+    var fullPath = await storageProvider.GetFullPathAsync(fileName);
+    return TypedResults.Ok(fullPath);
+});
+
+attachementsApiGroup.MapGet("info", async (IStorageProvider storageProvider, string fileName) =>
+{
+    var fullPath = await storageProvider.GetPropertiesAsync(fileName);
+    return TypedResults.Ok(fullPath);
+})
+.WithOpenApi();
+
+attachementsApiGroup.MapGet("read-uri", async (IStorageProvider storageProvider, string fileName, DateTime expirationDate) =>
+{
+    var readUri = await storageProvider.GetReadAccessUriAsync(fileName, expirationDate);
+    return TypedResults.Ok(readUri);
+})
+.WithOpenApi();
+
+attachementsApiGroup.MapPost(string.Empty, async (IFormFile file, IStorageProvider storageProvider, string? folder = null, bool overwrite = false) =>
+{
+    using var stream = file.OpenReadStream();
+    await storageProvider.SaveAsync(Path.Combine(folder ?? string.Empty, file.FileName), stream, overwrite);
+
+    return TypedResults.NoContent();
+})
+.DisableAntiforgery()
+.WithOpenApi();
+
+attachementsApiGroup.MapGet("content", async Task<Results<FileStreamHttpResult, NotFound>> (IStorageProvider storageProvider, string fileName) =>
+{
+    var attachment = await storageProvider.ReadAsStreamAsync(fileName);
+    if (attachment is null)
+    {
+        return TypedResults.NotFound();
+    }
+
+    return TypedResults.Stream(attachment, MimeUtility.GetMimeMapping(fileName));
+})
+.WithOpenApi();
+
+attachementsApiGroup.MapDelete(string.Empty, async (IStorageProvider storageProvider, string fileName) =>
+{
+    await storageProvider.DeleteAsync(fileName);
+    return TypedResults.NoContent();
+})
+.WithOpenApi();
 
 app.Run();
